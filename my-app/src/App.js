3083920151eb253f9ed1032364d49f23c6809ce2
doyelee0313 <doyelee0313@gmail.com>
2024-07-31 11:46:12 +0900

    event

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,15 +4,31 @@ import './App.css';
 function Header(props) {
   console.log(`props`, props, props.title)
   return <header>
-    <h1><a href="/">{props.title}</a></h1>
+    <h1><a href="/" onClick={function(event){
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
   </header>
+}
+
+//PROPS -> props 사용할때는 {props.title} 이렇게 괄호 안에 넣어야함
+function Article(props) {
+  return <article>
+  <h2>{props.title}</h2>
+  {props.body}
+  </article>
 }
 
 function Nav(props) {
   const lis = [];
   for(let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={`/read`+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={`/read`+t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id); //event.target -> event를 유발시킨 타켓
+      }}>{t.title} </a>
+    </li>)
   }
   return <nav>
     <ol>
@@ -21,26 +37,29 @@ function Nav(props) {
   </nav>
 }
 
-//props 사용할때는 {props.title} 이렇게 괄호 안에 넣어야함
-function Article(props) {
-  return <article>
-  <h2>{props.title}</h2>
-  {props.body}
-  </article>
-}
-
 function App() {
+  const mode = `Welcome`;
   const topics = [
     {id: 1, title:`html`, body:`html is ...`},
     {id: 2, title:`css`, body:`css is ...`},
     {id: 3, title:`js`, body:`js is ...`}
   ]
+  let content = null;
+  if (mode === `Welcome`) {
+    content = <Article title="welcome" body="hello, web"></Article>
+  } else if (mode === `READ`) {
+    content =  <Article title="read" body="hello, web"></Article>
+  }
   return (
     <div>
-      <Header title="React"></Header>
+      <Header title="React" onChangeMode={()=>{
+        alert(`header`);
+      }}></Header>
       <Header></Header>
-      <Nav topics={topics}></Nav>
-      <Article title="welcome" body="hello, web"></Article>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
+      {content}
     </div>
   );
 }
