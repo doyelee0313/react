d905e5bb0762c4ec65f922f452aea4a718295c6d
doyelee0313 <doyelee0313@gmail.com>
2024-07-31 15:47:42 +0900

    create

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {useState} from 'react';
 
 //COMPONENT -> in the form of function, must be in upper case
 function Header(props) {
@@ -26,7 +27,7 @@ function Nav(props) {
     lis.push(<li key={t.id}>
       <a id={t.id} href={`/read`+t.id} onClick={event=>{
         event.preventDefault();
-        props.onChangeMode(event.target.id); //event.target -> event를 유발시킨 타켓
+        props.onChangeMode(Number(event.target.id)); //event.target -> event를 유발시킨 타켓
       }}>{t.title} </a>
     </li>)
   }
@@ -37,29 +38,76 @@ function Nav(props) {
   </nav>
 }
 
+function Create(props) {
+  return <article>
+    <h2>create</h2>
+    <form onSubmit={event => {
+      event.preventDefault();
+      const title = event.target.title.value; //event.target = form tag
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder='title'></input></p>
+      <p><textarea name="body" placeholder='body'></textarea></p>
+      <p><input type="submit" value="Create"></input></p>    
+    </form>
+  </article>
+}
+
 function App() {
-  const mode = `Welcome`;
-  const topics = [
+  // const _mode = useState(`Welcome`); //useState는 배열을 나타냄
+  // console.log(_mode);
+  // const mode = _mode[0];
+  // const setMode = _mode[1];
+  const [mode, setMode] = useState(`Welcome`);
+  const [id, setId] = useState(null);
+  const [topics, setTopics] = useState([
     {id: 1, title:`html`, body:`html is ...`},
     {id: 2, title:`css`, body:`css is ...`},
     {id: 3, title:`js`, body:`js is ...`}
-  ]
+  ]);
+  const [nextId, setNextId] = useState(4);
   let content = null;
   if (mode === `Welcome`) {
     content = <Article title="welcome" body="hello, web"></Article>
   } else if (mode === `READ`) {
-    content =  <Article title="read" body="hello, web"></Article>
+    let title, body = null;
+    for (let i=0; i<topics.length; i++) {
+      console.log(topics[i].id, id)
+      if(topics[i].id === id){ //숫자인지 아닌지 잘 판별하기
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content =  <Article title={title} body={body}></Article>
+  } else if (mode === 'create'){
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body}
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
   }
   return (
     <div>
       <Header title="React" onChangeMode={()=>{
-        alert(`header`);
+        // alert(`header`);
+        setMode('Welcome')
       }}></Header>
       <Header></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        // alert(id);
+        setMode('READ')
+        setId(_id);
       }}></Nav>
       {content}
+      <a href="/create" onClick={event => {
+        event.preventDefault(); //stops the browser from navigating to /create
+        setMode('create');
+      }}>create</a>
     </div>
   );
 }
